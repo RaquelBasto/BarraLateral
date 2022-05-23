ec7195151e89eb1bfbc5ba0fc2b25d7e98eae801
RaquelBasto <bastoraquel941@gmail.com>
2022-05-23 15:19:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/barralateral/Bus.java
@@ -0,0 +1,17 @@
+package com.example.barralateral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+
+
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/barralateral/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.barralateral;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        //textView=findViewById(R.id.);
+        //toolbar=findViewById(R.id.toolbar);
+
+        //setSupportActionBar(toolbar);
+
+        getSupportActionBar().hide();
+        //
+
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new
+                ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+        //
+
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: break; case R.id.nav_bus:
+                Intent intent = new Intent(MainActivity.this, Bus.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_login: menu.findItem(R.id.nav_logout).setVisible(true);
+                menu.findItem(R.id.nav_profile).setVisible(true);
+                menu.findItem(R.id.nav_login).setVisible(false);
+                break;
+            case R.id.nav_logout: menu.findItem(R.id.nav_logout).setVisible(false);
+                menu.findItem(R.id.nav_profile).setVisible(false);
+                menu.findItem(R.id.nav_login).setVisible(true);
+                break;
+            case R.id.nav_share: Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show(); break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START); return true;
+    }
+}
